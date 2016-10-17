5929e6324a2706e603e2667852a0fc53574e9008
TMaster <github321>
2016-10-17 11:56:03 +0300

    Creating basic tree view

--- src/components/treeView.js
+++ src/components/treeView.js
@@ -2,11 +2,62 @@
  * Created by Tomer on 15/10/2016.
  */
 import React from 'react';
+import {Treebeard} from 'react-treebeard'
+
+const tempDataSource =
+    {
+        name :'root',
+        toggled: false,
+        children : [
+            {
+                name: "child1",
+                children : [
+                    {
+                        name : "child11"
+                    },
+                    {
+                        name : "child12"
+                    }
+                 ]
+            },
+            {
+                name : "child2"
+            },
+            {
+                name : "Tomer",
+                children : [
+                    {name: "Khalili"},
+                    {name : "Is Free"}
+
+                ]
+            }
+        ]
+
+    }
+
+    ;
 
 export default class TreeView extends React.Component{
 
+constructor(props){
+    super(props);
+    this.state = {};
+    this.onToggle = this.onToggle.bind(this);
+}
 
-    render(){ return ( <h1> here will be a tree view </h1>);
+    onToggle(node, toggled){
+        if(this.state.cursor){this.state.cursor.active = false;}
+        node.active = true;
+        if(node.children){ node.toggled = toggled; }
+        this.setState({ cursor: node });
+    }
+    render(){
+        return (
+            <Treebeard
+                data={tempDataSource}
+                onToggle={this.onToggle}
+                />
+        );
     }
 };
 
