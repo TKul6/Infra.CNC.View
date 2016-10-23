84d23487d6b74ae01c094f71c418a00a624bae90
TMaster <github321>
2016-10-23 12:42:30 +0300

    Adding basic search funcuality

--- src/components/main.js
+++ src/components/main.js
@@ -7,19 +7,16 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import TreeView from './treeView'
 import {deepOrange500} from 'material-ui/styles/colors';
+import LightTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 
-const muiTheme = getMuiTheme({
-    palette: {
-        accent1Color: deepOrange500,
-    },
-});
+const muiTheme = getMuiTheme({})
 
 export default class Main extends React.Component{
 
     render() {
-        return (<MuiThemeProvider>
-                    <div className='container'>
+        return (<MuiThemeProvider muiTheme={muiTheme}>
                     <div>
+                    <div className='container'>
                         <h1>Here will be the server name</h1>
                     </div>
                     <div>
--- src/components/treeView.js
+++ src/components/treeView.js
@@ -4,6 +4,8 @@
 import React from 'react';
 import {Treebeard, decorators} from 'react-treebeard'
 import Style from '../style/treeViewStyle.js'
+import TextField from 'material-ui/TextField'
+import * as filters from './filter';
 
 const tempDataSource =
     {
@@ -11,24 +13,24 @@ const tempDataSource =
         toggled: false,
         children : [
             {
-                name: "child1",
+                name: 'child1',
                 children : [
                     {
-                        name : "child11"
+                        name : 'child11'
                     },
                     {
-                        name : "child12"
+                        name : 'child12'
                     }
                  ]
             },
             {
-                name : "child2"
+                name : 'child2'
             },
             {
-                name : "Tomer",
+                name : 'Tomer',
                 children : [
-                    {name: "Khalili"},
-                    {name : "Is Free"}
+                    {name: 'Khalili'},
+                    {name : 'Is Free'}
 
                 ]
             }
@@ -43,15 +45,15 @@ decorators.Header = function(props){
 
     const style = props.style;
 
-    const iconStyle = { marginRight: '5px',
+    const iconStyle = {marginRight: '5px',
                         verticalAlign : 'middle'};
 
-    var nodeType = props.node.children ? "folder" : "insert_drive_file" ;
+    var nodeType = props.node.children ? 'folder' : 'insert_drive_file' ;
 
     return (
             <div style={style.base}>
                 <div style={style.title}>
-                <i className="material-icons" style={iconStyle}>{nodeType}</i>
+                <i className='material-icons' style={iconStyle}>{nodeType}</i>
                 {props.node.name}
                 </div>
                 </div>);
@@ -59,26 +61,47 @@ decorators.Header = function(props){
 };
 export default class TreeView extends React.Component{
 
-constructor(props){
-    super(props);
-    this.state = {};
-    this.onToggle = this.onToggle.bind(this);
+    
+    constructor(props) {
+        super(props);
+        this.state = {searchPaese : '',data : tempDataSource};
+        this.onToggle = this.onToggle.bind(this);
+        this.onFilter = this.onFilter.bind(this);
 }
 
+
+onFilter(e) {
+
+        this.setState({searchPaese : e.target.value});
+        const filter = e.target.value.trim();
+        if (filter) {
+            var filtered = filters.filterTree(tempDataSource, filter);
+            filtered = filters.expandFilteredNodes(filtered, filter);
+            this.setState({data: filtered});
+        }
+        else {
+            this.setState({data : tempDataSource})
+        }
+    }
+
     onToggle(node, toggled){
-        if(this.state.cursor){this.state.cursor.active = false;}
+        if (this.state.cursor) {this.state.cursor.active = false;}
         node.active = true;
-        if(node.children){ node.toggled = toggled; }
-        this.setState({ cursor: node });
+        if (node.children) { node.toggled = toggled; }
+        this.setState({cursor: node});
     }
     render(){
         return (
 <div>
+
     <h3>Tree View:</h3>
 
-
+<TextField  hintText=''
+      floatingLabelText='Search' value={this.state.searchPaese}
+      onChange={this.onFilter}/>
             <Treebeard
-                data={tempDataSource}
+
+                data={this.state.data}
                 onToggle={this.onToggle}
                 style={Style}
                 decorators={decorators}
