b82955cd06e7fa0b16c4a451807f263b7b05e57e
TMaster <github321>
2017-01-07 14:25:39 +0200

    Changing back to AppBar

--- src/components/applicationBar.js
+++ src/components/applicationBar.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import  AppBar  from 'material-ui/AppBar';
+import FontIcon from 'material-ui/FontIcon';
+import {blue100} from 'material-ui/styles/colors'
+
+const iconStyle = {
+     'margin-top': '16'
+}
 
 export default class ApplicationBar extends React.Component {
 
@@ -11,7 +17,8 @@ export default class ApplicationBar extends React.Component {
         console.log('rendering app bar');
         return (
             <div id='appBarPlaceHolder'>
-                <AppBar title='Cnc View' />
+                <AppBar title='Cnc View'  iconStyleLeft={iconStyle}
+                iconElementLeft={ <FontIcon className='material-icons white' color={blue100}>build</FontIcon>}/>
             </div>)
             ;
     }
