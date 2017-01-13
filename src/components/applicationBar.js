e1657d07183392bb25963b7aae6d489218e685c6
TMaster <github321>
2017-01-13 12:40:18 +0200

    Adding a url text filed to app bar

--- src/components/applicationBar.js
+++ src/components/applicationBar.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import  AppBar  from 'material-ui/AppBar';
 import FontIcon from 'material-ui/FontIcon';
-import {blue100} from 'material-ui/styles/colors'
+import {blue100, teal100, white} from 'material-ui/styles/colors';
+import TextField from 'material-ui/TextField';
 
 const iconStyle = {
-     'margin-top': '16'
+    'marginTop': '16px'
 }
+
+const labelStyles = {
+  
+  underlineStyle: {
+    borderColor: white,
+},
+  whiteText: {
+    color:white,
+  },
+  semiWhiteText: {
+      color:teal100,
+  },
+
+
+};
 
 export default class ApplicationBar extends React.Component {
 
@@ -18,7 +34,11 @@ export default class ApplicationBar extends React.Component {
         return (
             <div id='appBarPlaceHolder'>
                 <AppBar title='Cnc View'  iconStyleLeft={iconStyle}
-                iconElementLeft={ <FontIcon className='material-icons white' color={blue100}>build</FontIcon>}/>
+                iconElementLeft={ <FontIcon className='material-icons white' color={blue100}>build</FontIcon>}
+                children={<TextField hintText='http://localhost:4099/api/v1/cnc' floatingLabelText='Connect to server'
+                className='app-bar-component' underlineStyle={labelStyles.underlineStyle}
+                floatingLabelStyle={labelStyles.whiteText}
+                hintStyle={labelStyles.semiWhiteText} inputStyle={labelStyles.whiteText} />} />
             </div>)
             ;
     }
