37d68d90d1ba88b300ca6aa690088884a27d28fe
TMaster <github321>
2017-07-15 12:54:04 +0300

    Removing babellrc file

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
         test: /.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
       }
     ]
   },
